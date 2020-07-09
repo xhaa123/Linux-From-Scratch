8769075cb882358f1f65d39bd2f8e546a13ca4b8
xhaa123 <xhaa123@dingtalk.com>
2020-07-10 03:14:28 +0800

    Update 2-Entering-Chroot-and-Building-Additional-Tools.sh

--- lfs-scripts/2-Entering-Chroot-and-Building-Additional-Tools.sh
+++ lfs-scripts/2-Entering-Chroot-and-Building-Additional-Tools.sh
@@ -6,13 +6,10 @@ PKG=(
     'Libstdc++-from-GCC-10.1.0-Pass2'
     'Bison-3.6.4'
     'Gettext-0.20.2'
-    'Perl-5.30.3'
+    'Perl-5.32.0'
     'Python-3.8.3'
     'Texinfo-6.7'
     'Util-linux-2.35.2'
-    'Tcl-8.6.10'
-    'Expect-5.45.4'
-    'DejaGNU-1.6.2'
 )
 
 for script in "${PKG[@]}"; do
